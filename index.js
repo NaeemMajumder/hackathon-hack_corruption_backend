092b9bae5c0558aae58fdd83531ff84299a52897
naeem majumder <naeemmajumder00@gmail.com>
2025-02-12 12:53:09 +0600

    add mongo url

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 
-
+// all ai implementation
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const axios = require('axios')
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+
+// mongoose (2)
+const mongoose = require("mongoose");
+let mongo_url = process.env.MONGO_URL;
+main()
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+async function main() {
+  await mongoose.connect(mongo_url);
+}
 
 
 app.get('/',(req,res)=>{
